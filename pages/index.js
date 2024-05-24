d17079711d8408364a2af0c0ddc6bbc1082be68a
morishita <bafjkfgbka@gmail.com>
2024-05-24 16:08:23 +0700

    3rd commit

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home({ allPostsData }) {
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small>
-            </li>
+            </li>ss
           ))}
         </ul>
       </section>
