65bdc7fd5b8f505dab012f060704b83757cf671d
morishita <bafjkfgbka@gmail.com>
2024-05-25 19:17:57 +0700

    add time

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
-  const date = parseISO('2014-02-11T11:30:30');
+  const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
