f72dd2a5c1e5c9b7fcdbdb13ced2ac7f5a2018f7
morishita <bafjkfgbka@gmail.com>
2024-05-25 19:15:02 +0700

    parseISO

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return date //<time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  const date = parseISO('2014-02-11T11:30:30');
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
