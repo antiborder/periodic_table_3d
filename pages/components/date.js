99ba66316ebe44a8a386474ffec30e09cbe40e19
morishita <bafjkfgbka@gmail.com>
2024-06-01 20:37:00 +0700

    vercel test

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
+  // const date = parseISO('2014-02-11T11:30:30')
   return <time dateTime={dateString}>{dateString}</time>;
 }
