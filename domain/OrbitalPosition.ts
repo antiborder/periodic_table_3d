4a1093cbf30bcf56a7b2f7207eab5318c689557c
morishita <bafjkfgbka@gmail.com>
2024-07-23 08:54:11 +0900

    separate elementouch position

--- domain/OrbitalPosition.ts
+++ domain/OrbitalPosition.ts
@@ -1,5 +1,5 @@
 import elements from '../constants/elements'
-import {cartesianToCylindrical} from '../funcs/coordinateFuncs'
+import {cartesianToCylindrical, getOrbitalPositionX, getOrbitNumber} from '../funcs/coordinateFuncs'
 
 type Num3 = [number, number, number]
 
@@ -13,73 +13,11 @@ class OrbitalPosition {
 
     public orbitalPosition(): Num3 {
       return [
-        this.getOrbitalPositionX(),
-        +3 * this.getOrbitNumber(elements[this.atomicNumber].orbit.slice(-1)) - 6,
+        getOrbitalPositionX(this.atomicNumber),
+        +3 * getOrbitNumber(elements[this.atomicNumber].orbit.slice(-1)) - 6,
         -2 * parseInt(elements[this.atomicNumber].orbit.slice(0)),
       ]
     }
-
-    private getOrbitalPositionX(): number  {
-        let x = 0
-        let shellNumber = parseInt(elements[this.atomicNumber].orbit.slice(0, 1))
-        let orbitNumber = this.getOrbitNumber(elements[this.atomicNumber].orbit.slice(-1))
-        switch (orbitNumber) {
-          case 1:
-            if (elements[this.atomicNumber].atomicNumber === 2 || elements[this.atomicNumber].atomicNumber === 1) {
-              x = elements[this.atomicNumber].spiralColumn - 4
-            } else {
-              x = elements[this.atomicNumber].spiralColumn - 6
-            }
-            break
-          case 2:
-            if (shellNumber === 2 || shellNumber === 3) {
-              x = elements[this.atomicNumber].spiralColumn % 8
-            } else {
-              x = elements[this.atomicNumber].spiralColumn % 10
-            }
-            break
-          case 3:
-            if (
-                this.atomicNumber === 21 ||
-                this.atomicNumber === 39 ||
-                this.atomicNumber === 71 ||
-                this.atomicNumber === 103
-            ) {
-              x = 0
-            } else {
-              x = elements[this.atomicNumber].spiralColumn
-            }
-            break
-          default:
-            if (this.atomicNumber === 57 || this.atomicNumber === 89) {
-              x = 0
-            } else {
-              x = elements[this.atomicNumber].spiralColumn
-            }
-        }
-        return x
-      }
-
-
-      private getOrbitNumber (orbit): number  {
-        let orbitNumber = 0
-        switch (orbit) {
-          case "s":
-            orbitNumber = 1
-            break
-          case "p":
-            orbitNumber = 2
-            break
-          case "d":
-            orbitNumber = 3
-            break
-          case "f":
-            orbitNumber = 4
-            break
-          default:
-        }
-        return orbitNumber
-      }
 
       public radius (): number {
         return cartesianToCylindrical(this.orbitalPosition())[0]
--- funcs/coordinateFuncs.ts
+++ funcs/coordinateFuncs.ts
@@ -4,6 +4,7 @@ import TablePosition from "../domain/TablePosition"
 import OrbitalPosition from "../domain/OrbitalPosition"
 import CurledPosition from "../domain/CurledPosition"
 import DiscPosition from "../domain/DiscPosition"
+import ElementouchPosition from "../domain/ElementouchPosition"
 
 
 const ELEMENTOUCH_RADIUS_P = 2.0
@@ -43,26 +44,6 @@ const getTransition0to1Coordinate = (t: number, atomicNumber: number): Num3 => {
     cartesianCoordinate[2],
   ]
   return translatedCoordinate
-}
-
-const getOrbitNumber = (orbit): number => {
-  let orbitNumber = 0
-  switch (orbit) {
-    case "s":
-      orbitNumber = 1
-      break
-    case "p":
-      orbitNumber = 2
-      break
-    case "d":
-      orbitNumber = 3
-      break
-    case "f":
-      orbitNumber = 4
-      break
-    default:
-  }
-  return orbitNumber
 }
 
 const cylindricalToCartesian = ([radius, theta, z]: Num3): Num3 => {
@@ -114,25 +95,29 @@ const getOrbitalPositionX = (atomicNumber: number): number => {
   }
   return x
 }
+export {getOrbitalPositionX}
 
-const getElementouchPeriod = (atomicNumber: number): number => {
-  let period = 0
-  switch (elements[atomicNumber]["spiralRow"]) {
-    case 4:
-    case 6:
-    case 9:
-    case 12:
-      period = 10
+const getOrbitNumber = (orbit:string): number => {
+  let orbitNumber = 0
+  switch (orbit) {
+    case "s":
+      orbitNumber = 1
       break
-    case 8:
-    case 11:
-      period = 14
+    case "p":
+      orbitNumber = 2
+      break
+    case "d":
+      orbitNumber = 3
+      break
+    case "f":
+      orbitNumber = 4
       break
     default:
-      period = 8
   }
-  return period
+  return orbitNumber
 }
+export {getOrbitNumber}
+
 
 const getTransition1to2Coordinate = (t: number, atomicNumber: number): Num3 => {
   const curledPosition = new CurledPosition(atomicNumber)
@@ -154,12 +139,13 @@ const getTransition1to2Coordinate = (t: number, atomicNumber: number): Num3 => {
 
 const getTransition2to3Coordinate = (t: number, atomicnumber: number): Num3 => {
   const discPosition = new DiscPosition(atomicnumber)
-  const distance = getRadius3(atomicnumber) - ELEMENTOUCH_RADIUS_P
+  const elementouchPosition = new ElementouchPosition(atomicnumber)
+  const distance = elementouchPosition.radius() - ELEMENTOUCH_RADIUS_P
   const cartesianCoordinate = cylindricalToCartesian([
-    discPosition.radius() + (t - 2) * (getRadius3(atomicnumber) - discPosition.radius()),
+    discPosition.radius() + (t - 2) * (elementouchPosition.radius() - discPosition.radius()),
     discPosition.theta() +
-      Math.pow(t - 2, 1) * (getTheta3down(atomicnumber) - discPosition.theta()),
-      discPosition.z() + (t - 2) * (z3(atomicnumber) - discPosition.z()),
+      Math.pow(t - 2, 1) * (elementouchPosition.thetaUp() - discPosition.theta()),
+      discPosition.z() + (t - 2) * (elementouchPosition.z() - discPosition.z()),
   ])
   const translatedCoordinate: Num3 = [
     cartesianCoordinate[0],
@@ -169,83 +155,24 @@ const getTransition2to3Coordinate = (t: number, atomicnumber: number): Num3 => {
   return translatedCoordinate
 }
 
-const getRadius3 = (atomicNumber: number): number => {
-  let radius = 0
-  switch (elements[atomicNumber]["spiralRow"]) {
-    case 4:
-    case 6:
-    case 9:
-    case 12:
-      radius = ELEMENTOUCH_RADIUS_D
-      break
-    case 8:
-    case 11:
-      radius = ELEMENTOUCH_RADIUS_F
-      break
-    default:
-      radius = ELEMENTOUCH_RADIUS_P
-  }
-  return radius
-}
-const getTheta3down = (atomicNumber) => {
-  let baseTheta = elements[atomicNumber].spiralColumn - (1 % getElementouchPeriod(atomicNumber))
-  let adjustedTheta =
-    (baseTheta / getElementouchPeriod(atomicNumber)) * 2 * Math.PI -
-    (Math.PI * 4.1) / 8 +
-    Math.PI -
-    2 * Math.PI * elements[atomicNumber]["winding"]
-  return adjustedTheta
-}
-const getTheta3up = (atomicNumber) => {
-  let winding = 0
-  switch (elements[atomicNumber]["orbit"].slice(-1)) {
-    case "s":
-      winding = -1
-      break
-    case "p":
-      winding = getOrbitalPositionX(atomicNumber) === 0 ? -2 : -1
-      break
-    case "d":
-      winding = getOrbitalPositionX(atomicNumber) === 0 ? -2 : -1
-      break
-    case "f":
-      winding = getOrbitalPositionX(atomicNumber) === 0 ? -2 : -1
-      break
-    default:
-  }
-
-  let baseTheta = elements[atomicNumber].spiralColumn - (1 % getElementouchPeriod(atomicNumber))
-  let adjustedTheta =
-    (baseTheta / getElementouchPeriod(atomicNumber)) * 2 * Math.PI -
-    (Math.PI * 4.1) / 8 +
-    Math.PI +
-    2 * Math.PI * winding
-  return adjustedTheta
-}
-const z3 = (atomicNumber) =>
-  -elements[atomicNumber].spiralRow -
-  elements[atomicNumber].spiralColumn / getElementouchPeriod(atomicNumber)
-
 const getTransition3to4Coordinate = (t: number, atomicNumber: number): Num3 => {
   const orbitalPosition = new OrbitalPosition(atomicNumber)
-  const distance = getRadius3(atomicNumber) - ELEMENTOUCH_RADIUS_P
+  const elementouchPosition = new ElementouchPosition(atomicNumber)
+  const distance = elementouchPosition.radius() - ELEMENTOUCH_RADIUS_P
   const cartesianCoordinate = cylindricalToCartesian([
-    getRadius3(atomicNumber) +
-      Math.pow(t - 3, 0.5) * (orbitalPosition.radius() - getRadius3(atomicNumber)),
-    getTheta3up(atomicNumber) +
-      Math.pow(t - 3, 0.7) * (orbitalPosition.theta() - getTheta3up(atomicNumber)),
-    z3(atomicNumber) + (t - 3) * (orbitalPosition.z() - z3(atomicNumber)),
+    elementouchPosition.radius() +
+      Math.pow(t - 3, 0.5) * (orbitalPosition.radius() - elementouchPosition.radius()),
+      elementouchPosition.thetaUp() +
+      Math.pow(t - 3, 0.7) * (orbitalPosition.theta() - elementouchPosition.thetaUp()),
+    elementouchPosition.z() + (t - 3) * (orbitalPosition.z() - elementouchPosition.z()),
   ])
+
   const translatedCoordinate: Num3 = [
     cartesianCoordinate[0],
     cartesianCoordinate[1] - Math.abs(4 - t) * distance,
     cartesianCoordinate[2],
   ]
   return translatedCoordinate
-}
-
-const revalueTheta = (theta) => {
-  return Math.PI < theta ? theta - 2 * Math.PI : theta
 }
 
 const getTransition4to5Coordinate = (t: number, atomicNumber: number): Num3 => {
@@ -282,6 +209,7 @@ const getRotationAngle = (t: number, atomicNumber: number): Num3 => {
   const tablePosition = new TablePosition(atomicNumber)
   const curledPosition = new CurledPosition(atomicNumber)
   const discPosition = new DiscPosition(atomicNumber)
+  const elementouchPosition = new ElementouchPosition(atomicNumber)
   const theta0 = tablePosition.theta()
   if (t >= 0 && t <= 1) {
     const theta1 = curledPosition.thetaDown()
@@ -297,7 +225,7 @@ const getRotationAngle = (t: number, atomicNumber: number): Num3 => {
   } else if (t > 2 && t <= 3) {
     const baseTheta =
     discPosition.theta() +
-      Math.pow(t - 2, 1 / 2) * (getTheta3down(atomicNumber) - discPosition.theta())
+      Math.pow(t - 2, 1 / 2) * (elementouchPosition.thetaDown() - discPosition.theta())
     return [0, 0, baseTheta + (Math.PI * 1.1) / 2]
   } else if (t > 3 && t <= 4) {
     const revaluedTheta0 = theta0
@@ -306,8 +234,8 @@ const getRotationAngle = (t: number, atomicNumber: number): Num3 => {
       0,
       0,
       revalueTheta(
-        getTheta3up(atomicNumber) +
-          Math.pow(t - 3, 2) * (revaluedTheta0 - getTheta3up(atomicNumber)) +
+        elementouchPosition.thetaUp() +
+          Math.pow(t - 3, 2) * (revaluedTheta0 - elementouchPosition.thetaUp()) +
           (Math.PI * 1.1) / 2
       ) *
         (4 - t),
@@ -317,3 +245,7 @@ const getRotationAngle = (t: number, atomicNumber: number): Num3 => {
   }
 }
 export { getRotationAngle }
+
+const revalueTheta = (theta) => {
+  return Math.PI < theta ? theta - 2 * Math.PI : theta
+}
